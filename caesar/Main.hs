b52af89d2aa95a36800a71e1ce8fb328a2ce8a28
Brian Hamrick <brian.c.hamrick@gmail.com>
2013-11-13 06:03:33 -0800

    Options for caesar

--- caesar/Main.hs
+++ caesar/Main.hs
@@ -7,15 +7,55 @@ import System.Environment
 
 import GHC.Exts (sortWith)
 
+data Options = Options
+  { o_all :: Bool
+  , o_rated :: Bool
+  } deriving (Eq, Show, Ord)
+
+defaultOptions = Options
+  { o_all = False
+  , o_rated = True
+  }
+
 main :: IO ()
 main = do
     args <- getArgs
-    let shifts = caesarShifts . unwords $ args
-    let ratedShifts = zip3 [0..25] shifts (map rating shifts)
-    let sortedShifts = sortWith (negate . thd) ratedShifts
-            where thd (x,y,z) = z
-    mapM_ printLine sortedShifts
-            where printLine (offset,shifted,score) = putStrLn $ (show offset) ++ "\t" ++ shifted ++ "\t" ++ (show score)
+    let (opts, args') = parseArgs defaultOptions args
+    let shifts = caesarShifts . unwords $ args'
+    if o_rated opts
+        then goRated opts shifts
+        else goUnrated opts shifts
+
+goRated :: Options -> [String] -> IO ()
+goRated opts shifts = do
+    let ratedShifts = sortWith (negate . thd) $ zip3 [0..25] shifts (map rating shifts)
+            where thd (x, y, z) = z
+    if o_all opts
+        then mapM_ printTriple ratedShifts
+        else printTriple (head ratedShifts)
+
+goUnrated :: Options -> [String] -> IO ()
+goUnrated opts shifts = do
+    let pairedShifts = zip [0..25] shifts
+    mapM_ printPair pairedShifts
+
+printPair (offset, shifted) = putStrLn $ (show offset) ++ "\t" ++ shifted
+printTriple (offset, shifted, score) = putStrLn $ (show offset) ++ "\t" ++ shifted ++ "\t" ++ (show score)
+
+parseArgs :: Options -> [String] -> (Options, [String])
+parseArgs o [] = (o, [])
+parseArgs o (a:as) = if head a == '-'
+    then parseArgs (updateArgs a o) as
+    else (o', a:as') where (o', as') = parseArgs o as
+
+updateArgs :: String -> Options -> Options
+updateArgs [] o = o
+updateArgs (c:cs) o = case c of
+    'a' -> updateArgs cs (o { o_all = True })
+    'u' -> updateArgs cs (o { o_rated = False })
+    'o' -> updateArgs cs (o { o_rated = False })
+    'r' -> updateArgs cs (o { o_rated = True })
+    _ -> updateArgs cs o
 
 usage :: IO ()
 usage = do
